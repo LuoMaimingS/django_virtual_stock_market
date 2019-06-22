51f37d9d6998ce6c63aff26e757f5c6b353ddf46
lei yu <mp1833013@smail.nju.edu.cn>
2019-06-22 18:42:52 +0800

    work

--- VirtualStockMarket/settings.py
+++ VirtualStockMarket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blooming-sea-93432.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
